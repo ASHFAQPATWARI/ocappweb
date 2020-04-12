6a02f54a8d3ce29e79d9e4e03500cc779f086a44
ashfaq patwari <ashfaqpatwari@gmail.com>
2020-04-12 09:46:16 +0530

    initial

---
+++ ocappweb/flutter_service_worker.js
@@ -0,0 +1,39 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "index.html": "5c43a34723ecc3d299491efc0ec61f4a",
+"/": "5c43a34723ecc3d299491efc0ec61f4a",
+"main.dart.js": "46c2dfe189646209b4744fad5e47a9bb",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"manifest.json": "d2577122da009c58a31f2181e63aafdf",
+"assets/LICENSE": "e00c2e6b8e32e47284c57ff3b91f2eee",
+"assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
+"assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
