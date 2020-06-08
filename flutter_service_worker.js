451db1cc16405934d3e72b022089dc6ddc41ab71
ashfaq patwari <ashfaqpatwari@gmail.com>
2020-06-08 21:38:42 +0530

    after splash logo

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,13 +3,13 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "836056e99a0a3f18ccaeafa7919ea830",
 "/": "836056e99a0a3f18ccaeafa7919ea830",
-"main.dart.js": "9f1d7115e59a10b7b73cb01c18deb640",
+"main.dart.js": "d2e97996136e6642b1b90f0b2329a755",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"manifest.json": "65e58915975c854a33f1abe784c24c5f",
-"assets/LICENSE": "9000d8cadae9f747f87feb46407ca085",
-"assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
+"manifest.json": "76cb0f56543d384a13a2388d681e7449",
+"assets/LICENSE": "a15235118f95a5695d23aff3348fe01c",
+"assets/AssetManifest.json": "9b6531d365050e45aa9804818bb6cf73",
 "assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
 };
